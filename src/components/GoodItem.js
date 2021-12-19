da523a968b6e08763998248b7e1ae5d82850706e
YourContender <dream-boy123@ukr.net>
2021-12-19 13:45:43 +0200

    add goods list

---
+++ src/components/GoodItem.js
@@ -0,0 +1,23 @@
+import '../index.css'
+
+function GoodItem(props) {
+    const {id, name, description, price, full_background} = props;
+
+    return (
+        <div className="card" id={id} style={{backgroundColor: 'rgb(247, 247, 41)'}}>
+            <div className="card-image">
+                <img src={full_background} alt={name} />
+            </div>
+            <div className="card-content">
+                <span className="card-title">{name}</span>
+                <p>{description}</p>
+            </div>
+            <div className="card-action">
+                <button className="btn">Купить</button>
+                <p className="right" style={{fontSize: '20px', marginTop: '5px'}}>{price} UAH</p>
+            </div>
+        </div>
+    )
+}
+
+export default GoodItem;
